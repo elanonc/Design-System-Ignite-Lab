c1bfb3f37328c4976edd73d7aee83e6fc0bc55b4
Elano N. Caitano <nunes.elano14@gmail.com>
2022-10-16 22:37:14 -0300

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react';
+import "./styles/global.css";
+
+
+export function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <h1 className="font-bold text-2xl">Hello World</h1>
+  )
+}
