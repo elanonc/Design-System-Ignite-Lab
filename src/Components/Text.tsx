518578f6b291563150fc653039664542fc2415f8
Elano N. Caitano <nunes.elano14@gmail.com>
2022-10-16 23:22:43 -0300

    Componente text finalizado

--- src/Components/Text.tsx
+++ src/Components/Text.tsx
@@ -1,13 +1,18 @@
+import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+import { ReactNode } from "react";
 
-interface TextProps {
+export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
-    children: string;
+    children: ReactNode;
+    asChild?: boolean;
 }
 
-export function Text({ size = 'md'} : TextProps){
+export function Text({ size = 'md', children, asChild } : TextProps){
+    const Comp = asChild ? Slot : 'span';
+    
     return (
-        <span 
+        <Comp 
             className={clsx(
                 "text-gray-100 font-sans",
                 {
@@ -17,7 +22,7 @@ export function Text({ size = 'md'} : TextProps){
                 }
             )}
         >
-            Hello World
-        </span>
+            {children}
+        </Comp>
     )
 }
