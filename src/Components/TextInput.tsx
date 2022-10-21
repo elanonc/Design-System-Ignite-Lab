9009f58f88d2879a47a5750d9d898f02da6e7bc3
Elano N. Caitano <nunes.elano14@gmail.com>
2022-10-20 22:49:29 -0300

    Ajustes TextInput

--- src/Components/TextInput.tsx
+++ src/Components/TextInput.tsx
@@ -1,18 +1,34 @@
-import { clsx } from 'clsx';
-import { InputHTMLAttributes } from "react";
+// import { clsx } from 'clsx';
+import { InputHTMLAttributes, ReactNode } from "react";
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement>{
-    placeholder?: string
+export interface TextInputRootProps {
+    children: ReactNode;
 }
 
-export function TextInput(props: TextInputProps){
+function TextInputRoot(props: TextInputRootProps) {
+    return (
+        <div className="flex items-center gap-3 py-4 px-3 rounded w-full bg-gray-800 focus-within:ring-2 ring-cyan-300">
+            {props.children}
+        </div>
+        ) 
+}
+
+// function TextInputIcon() {}
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+export function TextInputInput(props: TextInputInputProps){
 
     return (
         <input 
-            className={clsx(
-                "py-4 px-3 rounded w-full bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400"
-            )}
-            {...props}
+            className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+                {...props}
         />
     )
+}
+
+export const TextInput = {
+    Root: TextInputRoot,
+    Input: TextInputInput,
+
 }
